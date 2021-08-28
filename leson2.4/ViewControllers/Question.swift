2910168d5f6e5b166ff223b9fa56955f8aef0c2d
Nikita12G <gulyi08@mail.ru>
2021-08-28 18:56:19 +0700

    Second

--- leson2.4/ViewControllers/Question.swift
+++ leson2.4/ViewControllers/Question.swift
@@ -9,6 +9,37 @@ struct Question {
     let title: String
     let answers: [Answer]
     let type: ResponseType
+    
+    static func getQuestion() -> [Question] {
+        [
+            Question(title: "Какую пищу вы предпочитаете?",
+                     answers: [
+                        Answer (title: "Стейк", animal: .dog),
+                        Answer (title: "Рыба", animal: .cat),
+                        Answer (title: "Морковь", animal: .rabbit),
+                        Answer (title: "Кукуруза", animal: .turtle)
+                     ], type: .single
+            ),
+            Question(title: "Что вам нравится больше?",
+                     answers: [
+                        Answer (title: "Плавать", animal: .dog),
+                        Answer (title: "Спать", animal: .cat),
+                        Answer (title: "Обниматься", animal: .rabbit),
+                        Answer (title: "Есть", animal: .turtle)
+                     ],
+                     type: .multiple
+            ),
+            Question(title: "Любите ли вы поездки на машине?",
+                     answers: [
+                        Answer (title: "Ненавижу", animal: .cat),
+                        Answer (title: "Нервничаю", animal: .rabbit),
+                        Answer (title: "Не замечаю", animal: .turtle),
+                        Answer (title: "Обажаю", animal: .dog)
+                     ],
+                     type: .ranged
+            ),
+        ]
+    }
 }
 
 struct Answer {
