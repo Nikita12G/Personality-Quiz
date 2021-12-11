7300aaa8e8ff1b73f365076be9b786f131b3f332
Nikita12G <87696400+Nikita12G@users.noreply.github.com>
2021-12-11 11:18:24 +0700

    Personality Quiz

---
+++ PersonalityQuiz/PersonalityQuiz/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  IntroductionViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Никита Гуляев on 05.09.2021.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {}
+}
+
